cd1e390cc0dcb7ff9bd242ee6ab60cb37f29e6a7
Louis Pilfold <louis@lpil.uk>
2017-07-03 22:20:31 +0100

    Tests: Check body for substrings

--- test/Support.hs
+++ test/Support.hs
@@ -1,6 +1,25 @@
-module Support where
+module Support
+  ( testAppConfig
+  , bodyContains
+  ) where
 
 import Captain (AppConfig(..))
+import Data.ByteString (isInfixOf)
+import Data.ByteString.Lazy (toStrict)
+import Test.Hspec.Wai.Matcher (Body, MatchBody(..))
 
 testAppConfig :: AppConfig
 testAppConfig = AppConfig {useLogger = False}
+
+{-| Check whether a response body contains a given substring.
+-}
+bodyContains :: Body -> MatchBody
+bodyContains subString = MatchBody bodyContains'
+  where
+    bodyContains' _ body =
+      if toStrict subString `isInfixOf` toStrict body
+        then Nothing
+        else Just $ errorMsg body
+    errorMsg body =
+      unlines
+        ["Body did not contain `" ++ show subString ++ "`", "Body:", show body]
--- test/WebSpec.hs
+++ test/WebSpec.hs
@@ -3,10 +3,11 @@
 module WebSpec where
 
 import Captain (appWith)
-import Support (testAppConfig)
+import Support (bodyContains, testAppConfig)
 import Test.Hspec
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 import Test.Hspec.Wai
+import Test.Hspec.Wai.Matcher
 import Web.Spock (spockAsApp)
 
 main :: IO ()
@@ -16,7 +17,5 @@ spec :: Spec
 spec =
   with (spockAsApp $ appWith testAppConfig) $
   describe "GET /" $
-  it "serves the home page" $ do
-    let resp = get "/"
-    resp `shouldRespondWith` 200
-    resp `shouldRespondWith` "Hello, Sailor!"
+  it "serves the home page" $
+  get "/" `shouldRespondWith` 200 {matchBody = bodyContains "Hello, Sailor!"}
